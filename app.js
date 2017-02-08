240c8d5bf5a8cd52d2d574fd7de1395b75e73aae
Zachariah Nagatani <zachnagatani@gmail.com>
2017-02-08 13:25:58 -0800

    Fix: IP Parsing

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express'),
       app = express(),
       port = process.env.PORT || 8000;
-
 app.get('/', (req, res) => {
     // Respond with the ip address, the first language, and parse the user-agent header to get the operating system details
     res.json({
-        ipaddress: req.ip,
+        ipaddress: req.headers['x-forwarded-for'] ||
+                   req.connection.remoteAddress ||
+                   req.socket.remoteAddress ||
+                   req.connection.socket.remoteAddress,
         language: req.acceptsLanguages()[0],
         os: req.headers['user-agent'].split(/[()]/)[1].slice(0, -1)
     });
